e6ceb3044ae007f91c8b398482a6bb9bf6b9f2be
Deep-1017 <12002080601017@adit.ac.in>
2025-11-04 11:09:07 +0530

    While Loop

---
+++ 04_Chapter/02_while.c
@@ -0,0 +1,16 @@
+// write a program to print the numbers between 5 to 10
+
+#include<stdio.h>
+
+int main() {
+    int num = 5;
+
+    while (num <= 10)
+    {
+        printf("%d\n", num);
+        num++;
+    }
+    
+
+    return 0;
+}
